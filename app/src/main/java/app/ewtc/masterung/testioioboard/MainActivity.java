781900431ed7b28029552f295297ce59244a3db7
MasterUNG <phrombutr@gmail.com>
2017-08-09 14:05:19 +0700

    Initial commit

---
+++ app/src/main/java/app/ewtc/masterung/testioioboard/MainActivity.java
@@ -0,0 +1,65 @@
+package app.ewtc.masterung.testioioboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import ioio.lib.api.exception.ConnectionLostException;
+import ioio.lib.util.BaseIOIOLooper;
+import ioio.lib.util.IOIOLooper;
+import ioio.lib.util.android.IOIOActivity;
+
+public class MainActivity extends IOIOActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }   // Main Method
+
+
+    class Looper extends BaseIOIOLooper {
+
+        @Override
+        protected void setup() throws ConnectionLostException, InterruptedException {
+//            super.setup();
+
+            //Check IOIO Connected
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(MainActivity.this, "Connected IOIO OK", Toast.LENGTH_LONG).show();
+                    Log.d("9AugV1", "Connected IOIO OK");
+                }
+            });
+
+
+        }   // setup
+
+        @Override
+        public void loop() throws ConnectionLostException, InterruptedException {
+//            super.loop();
+
+            try {
+
+                Thread.sleep(100);
+
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, "e ==> " + e.toString(), Toast.LENGTH_LONG).show();
+                Log.d("9AugV1", "e ==> " + e.toString());
+            }
+
+
+        }   // loop
+
+    }   // Looper Class
+
+    protected IOIOLooper createIOIOLooper() {
+
+        return new Looper();
+    }
+
+
+
+}   // Main Class
